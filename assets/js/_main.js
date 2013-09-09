12d65869da7cedb9b43d0ee27cdbfd2685d92e1f
Michael Rose <est.michael@gmail.com>
2013-09-09 10:12:07 -0400

    Init Gruntjs build script

---
+++ assets/js/_main.js
@@ -0,0 +1,36 @@
+/*! Plugin options and other jQuery stuff */
+
+// FitVids options
+$(function() {
+	$("article").fitVids();
+});
+
+// Table of Contents toggle
+$(function() {
+  $(".toc h3").click(function () {
+    $("#drawer").toggleClass("hidden");
+  });
+});
+
+// Add lightbox class to all image links
+$("a[href$='.jpg'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
+
+// Magnific-Popup options
+$(document).ready(function() {
+  $('.image-popup').magnificPopup({
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
+    },
+    removalDelay: 300, // Delay in milliseconds before popup is removed
+    // Class that is added to body when popup is open. 
+    // make it unique to apply your CSS animations just to this exact popup
+    mainClass: 'mfp-fade'
+  });
+});
