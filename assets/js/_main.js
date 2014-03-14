db2981d253534bf2b03b8341e2ee490ffd1dada0
Michael Rose <est.michael@gmail.com>
2014-03-14 08:37:12 -0400

    Add .JPG and .jpeg extensions to Magnific Popup lightbox call. Fixes #33

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -44,7 +44,7 @@ $(function() {
 });
 
 // Add lightbox class to all image links
-$("a[href$='.jpg'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
+$("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
 
 // Magnific-Popup options
 $(document).ready(function() {
