23cb7ecaebf080a42295097c782e5d3f3b1d3fcf
Michael Rose <est.michael@gmail.com>
2014-07-31 13:09:32 -0400

    Convert .less to .scss

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,19 +13,6 @@ module.exports = function(grunt) {
         '!assets/js/scripts.min.js'
       ]
     },
-    recess: {
-      dist: {
-        options: {
-          compile: true,
-          compress: true
-        },
-        files: {
-          'assets/css/main.min.css': [
-            'assets/less/main.less'
-          ]
-        }
-      }
-    },
     uglify: {
       dist: {
         files: {
@@ -61,12 +48,6 @@ module.exports = function(grunt) {
       }
     },
     watch: {
-      less: {
-        files: [
-          'assets/less/*.less'
-        ],
-        tasks: ['recess']
-      },
       js: {
         files: [
           '<%= jshint.all %>'
@@ -76,7 +57,6 @@ module.exports = function(grunt) {
     },
     clean: {
       dist: [
-        'assets/css/main.min.css',
         'assets/js/scripts.min.js'
       ]
     }
@@ -87,14 +67,12 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-recess');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-svgmin');
 
   // Register tasks
   grunt.registerTask('default', [
     'clean',
-    'recess',
     'uglify',
     'imagemin',
     'svgmin'
