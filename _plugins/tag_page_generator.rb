e373e34ef89b01d713d3be4c61c0b12752403f5d
Balint Neray <balintneray@Balints-MacBook-Pro.local>
2014-10-29 09:14:36 -0400

    oo

---
+++ _plugins/tag_page_generator.rb
@@ -0,0 +1,35 @@
+module Jekyll
+  class TagIndex < Page
+    def initialize(site, base, dir, tag)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'index.html'
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
+      self.data['tag'] = tag
+      tag_title_prefix = site.config['tag_title_prefix'] || 'Tagged: '
+      tag_title_suffix = site.config['tag_title_suffix'] || '&#8211;'
+      self.data['title'] = "#{tag_title_prefix}#{tag}"
+      self.data['excerpt'] = "An archive of posts tagged #{tag}."
+    end
+  end
+  class TagGenerator < Generator
+    safe true
+    def generate(site)
+      if site.layouts.key? 'tag_index'
+        dir = site.config['tag_dir'] || 'tag'
+        site.tags.keys.each do |tag|
+          # replace spaces with hyphens and downcase tag
+          write_tag_index(site, File.join(dir, tag.gsub(/[^[:alnum:]]+/, '-').downcase), tag)
+        end
+      end
+    end
+    def write_tag_index(site, dir, tag)
+      index = TagIndex.new(site, site.source, dir, tag)
+      index.render(site.layouts, site.site_payload)
+      index.write(site.dest)
+      site.pages << index
+    end
+  end
+end
