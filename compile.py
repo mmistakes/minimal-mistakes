bab4076e8daa72083ed6087b78941d82c4b7bdc0
Phillis Tang <root@nsit-dhcp-205-208-059-044.uchicago.edu>
2015-04-22 12:19:46 -0500

    Moved text for featured project to bottom of circles

--- compile.py
+++ compile.py
@@ -3,6 +3,10 @@ import os,sys
 dirname=os.path.dirname
 filepath=(dirname(dirname(os.path.abspath(__file__))))
 sys.path.append(filepath)
+if len(sys.argv) ==2 :
+  dest = sys.argv[1]
+else :
+  dest = 'compiled'
 
 from jinja2 import Environment, PackageLoader
 env = Environment(loader=PackageLoader('cdiswebsite', 'template'))
@@ -10,6 +14,6 @@ for filename in os.listdir('template'):
     if filename!='base.html':
         template = env.get_template(filename)
         content=template.render()
-        f=open(os.path.join('compiled',filename),'w')
+        f=open(os.path.join(dest ,filename),'w')
         f.write(content.encode('utf-8'))
         f.close()
