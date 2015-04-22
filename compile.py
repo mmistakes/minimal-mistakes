06c26266cf5efba1984cc60e47a5617284eb8dfa
Phillis Tang <phillis.tt@gmail.com>
2015-04-22 10:06:42 -0500

    Added compiled static pages

--- compile.py
+++ compile.py
@@ -10,6 +10,6 @@ for filename in os.listdir('template'):
     if filename!='base.html':
         template = env.get_template(filename)
         content=template.render()
-        f=open(os.path.join('/Library/WebServer/Documents',filename),'w')
+        f=open(os.path.join('compiled',filename),'w')
         f.write(content.encode('utf-8'))
         f.close()
