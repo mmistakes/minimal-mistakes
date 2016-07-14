cd79ac9a640da28a5c56ebaa87d51e147535e500
Matthew Aguirre <matt.aguirre@gmail.com>
2016-07-14 10:24:10 -0400

    Fixed condition that should be notted.

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
   $(".sticky").Stickyfill();
 
   var stickySideBar = function(){
-    var show = $(".author__urls-wrapper button").length === 0 ? $(window).width() > 1024 : $(".author__urls-wrapper button").is(":visible");
+    var show = $(".author__urls-wrapper button").length === 0 ? $(window).width() > 1024 : !$(".author__urls-wrapper button").is(":visible");
     // console.log("has button: " + $(".author__urls-wrapper button").length === 0);
     // console.log("Window Width: " + windowWidth);
     // console.log("show: " + show);
