57a4324fa04086ee8c035a6969d3c415f250e556
Michael Rose <est.michael@gmail.com>
2016-11-03 11:52:04 -0400

    Move `gh-pages` branch files into `/docs` and add test files - Jekyll ignore `/docs` and `/test` folders when using from root - Update Staticman config to point to correct branch and data file location - Replace `{{ base_path }}` references with `absolute_url` filter - Update documentation

---
+++ docs/assets/js/plugins/jquery.greedy-navigation.js
@@ -0,0 +1,72 @@
+/*
+* Greedy Navigation
+*
+* http://codepen.io/lukejacksonn/pen/PwmwWV
+*
+*/
+
+var $nav = $('#site-nav');
+var $btn = $('#site-nav button');
+var $vlinks = $('#site-nav .visible-links');
+var $hlinks = $('#site-nav .hidden-links');
+
+var breaks = [];
+
+function updateNav() {
+
+  var availableSpace = $btn.hasClass('hidden') ? $nav.width() : $nav.width() - $btn.width() - 30;
+
+  // The visible list is overflowing the nav
+  if($vlinks.width() > availableSpace) {
+
+    // Record the width of the list
+    breaks.push($vlinks.width());
+
+    // Move item to the hidden list
+    $vlinks.children().last().prependTo($hlinks);
+
+    // Show the dropdown btn
+    if($btn.hasClass('hidden')) {
+      $btn.removeClass('hidden');
+    }
+
+  // The visible list is not overflowing
+  } else {
+
+    // There is space for another item in the nav
+    if(availableSpace > breaks[breaks.length-1]) {
+
+      // Move the item to the visible list
+      $hlinks.children().first().appendTo($vlinks);
+      breaks.pop();
+    }
+
+    // Hide the dropdown btn if hidden list is empty
+    if(breaks.length < 1) {
+      $btn.addClass('hidden');
+      $hlinks.addClass('hidden');
+    }
+  }
+
+  // Keep counter updated
+  $btn.attr("count", breaks.length);
+
+  // Recur if the visible list is still overflowing the nav
+  if($vlinks.width() > availableSpace) {
+    updateNav();
+  }
+
+}
+
+// Window listeners
+
+$(window).resize(function() {
+  updateNav();
+});
+
+$btn.on('click', function() {
+  $hlinks.toggleClass('hidden');
+  $(this).toggleClass('close');
+});
+
+updateNav();
---
+++ test/_posts/2012-03-15-layout-header-image-external.md
@@ -0,0 +1,17 @@
+---
+title: "Layout: Header Image (External URL)"
+header:
+  image: https://farm5.staticflickr.com/4140/4939863887_84705982fd_b.jpg
+categories:
+  - Layout
+  - Uncategorized
+tags:
+  - edge case
+  - featured image
+  - image
+  - layout
+---
+
+This post should display a **header image**, if the theme supports it.
+
+Featured image is an external asset and should load.
