28c0105016c043030a1b3b39cdb5690b2cd8aab4
jluccisano <joseph.luccisano@gmail.com>
2017-02-12 17:22:00 +0100

    Enable sticky sidebar - Hide sidebar on small screens

--- _assets/js/_main.js
+++ _assets/js/_main.js
@@ -13,7 +13,6 @@ $(document).ready(function(){
   // init smooth scroll
   $("a").smoothScroll({offset: -20});
 
-
   // add lightbox class to all image links
   $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
 
