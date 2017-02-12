2771df5279dc75170c4c6071a85f0c04826f5ad6
jluccisano <joseph.luccisano@gmail.com>
2017-02-12 17:22:00 +0100

    Make footer stick to the bottom of the page - Use JavaScript to determine height of `.page_footer` to apply padding to `body` avoiding any content overlap - Absolute position `.page_footer` to bottom of the page

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -4,6 +4,24 @@
 
 $(document).ready(function(){
 
+  // Sticky footer
+  var bumpIt = function() {
+      $('body').css('margin-bottom', $('.page__footer').outerHeight(true));
+    },
+    didResize = false;
+
+  bumpIt();
+
+  $(window).resize(function() {
+    didResize = true;
+  });
+  setInterval(function() {
+    if(didResize) {
+      didResize = false;
+      bumpIt();
+    }
+  }, 250);
+
   // FitVids init
   $("#main").fitVids();
 
