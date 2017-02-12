f4abebc64e8aac9243cf88948d78fe9be04c54d9
jluccisano <joseph.luccisano@gmail.com>
2017-02-12 17:22:00 +0100

    Gruntify stylesheets

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function(grunt) {
       all: [
         'Gruntfile.js',
         'assets/js/*.js',
-        'assets/js/plugins/*.js',
+        '!assets/js/plugins/*.js',
         '!assets/js/scripts.min.js'
       ]
     },
@@ -22,6 +22,9 @@ module.exports = function(grunt) {
         files: {
           'assets/css/main.min.css': [
             'assets/less/main.less'
+          ],
+          'assets/css/ie.min.css': [
+            'assets/less/ie.less'
           ]
         }
       }
