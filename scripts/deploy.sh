fd4fc15ad72060e434170a3490baa3d754c75092
jluccisano <joseph.luccisano@gmail.com>
2017-02-12 17:22:00 +0100

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,10 +12,9 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]
 fi
 
 echo "Starting to update master\n"
-pwd
 
 #copy data we're interested in to other place
-cp -R --verbose /home/travis/build/jluccisano/jluccisano.github.io/_site $HOME/_site
+cp -R --verbose _site $HOME/_site
 
 # Save some useful information
 REPO=`git config remote.origin.url`
@@ -25,38 +24,30 @@ SHA=`git rev-parse --verify HEAD`
 # Clone the existing master for this repo into out/
 # Create a new empty branch if master doesn't exist yet (should only happen on first deply)
 echo "cloning\n"
-pwd
-
-git clone $REPO out
-ls -la
 # Now let's go have some fun with the cloned repo
-cd out
-pwd
-
-ls -la
+git clone --verbose $REPO $HOME/out
+cd $HOME/out
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
-ls -la
 # Clean out existing contents
 echo "deleting\n"
-rm --verbose -rf out/**/* || exit
+rm --verbose -rf $HOME/out/**/* || exit
 #go into directory and copy data we're interested in to that directory
 
-cd out
-ls -la
+cd $HOME/out
 cp --verbose -Rf $HOME/_site/* .
-ls -la
-pwd
+
+
 
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if git diff --quiet; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
-pwd
+#if git diff --quiet; then
+#    echo "No changes to the output on this push; exiting."
+#    exit 0
+#fi
+
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
@@ -70,11 +61,11 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
-chmod 600 ../deploy_key
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in /home/travis/build/jluccisano/jluccisano.github.io/deploy_key.enc -out /home/travis/build/jluccisano/jluccisano.github.io/deploy_key -d
+chmod 600 /home/travis/build/jluccisano/jluccisano.github.io/deploy_key
 eval `ssh-agent -s`
-ssh-add ../deploy_key
-pwd
+ssh-add /home/travis/build/jluccisano/jluccisano.github.io/deploy_key
+#pwd
 
 # Now that we're all set up, we can push.
 echo "pushing\n"
