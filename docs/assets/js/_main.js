6b9afaa8b7f072e4c644d2f199dd2d320d83f407
Michael Rose <est.michael@gmail.com>
2017-08-04 11:55:10 -0400

    Fix disappearing author sidebar links - Close #1136

--- docs/assets/js/_main.js
+++ docs/assets/js/_main.js
@@ -27,7 +27,7 @@ $(document).ready(function(){
 
   // Follow menu drop down
   $(".author__urls-wrapper button").on("click", function() {
-    $(".author__urls").fadeToggle("fast", function() {});
+    $(".author__urls").toggleClass("is--visible");
     $(".author__urls-wrapper button").toggleClass("open");
   });
 
