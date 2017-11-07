028d4f8d0b97b1215c6888e653b5024a4c118eec
Giulio Troccoli-Allard <gtroccoli-allard@lrb.co.uk>
2017-11-07 17:33:22 +0000

    Added a better styled resume

---
+++ assets/resume/js/main.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function($) {
+
+    $('.level-bar-inner').css('width', '0');
+    
+    $(window).on('load', function() {
+
+        $('.level-bar-inner').each(function() {
+        
+            var itemWidth = $(this).data('level');
+            
+            $(this).animate({
+                width: itemWidth
+            }, 800);
+            
+        });
+
+    });
+   
+    
+
+});
---
+++ assets/resume/js/min/main-min.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function($){$(".level-bar-inner").css("width","0"),$(window).on("load",function(){$(".level-bar-inner").each(function(){var n=$(this).data("level");$(this).animate({width:n},800)})})});
