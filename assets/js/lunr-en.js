19f6d3274582bfc670bad20ec4a1309d5a36b7dd
Michael Rose <est.michael@gmail.com>
2017-11-15 15:50:29 -0500

    Replace hardcoded text with strings stored in `_data` file

--- assets/js/lunr-en.js
+++ assets/js/lunr-en.js
@@ -1,6 +1,6 @@
 ---
-
 ---
+
 var idx = lunr(function () {
   this.field('title', {boost: 10})
   this.field('excerpt')
@@ -58,26 +58,26 @@ $(document).ready(function() {
     var query = $(this).val();
     var result = idx.search(query);
     resultdiv.empty();
-    resultdiv.prepend('<p>'+result.length+' Result(s) found</p>');
+    resultdiv.prepend('<p>'+result.length+' {{ site.data.ui-text[site.locale].results_found | default: "Result(s) found" }}</p>');
     for (var item in result) {
       var ref = result[item].ref;
       if(store[ref].teaser){
         var searchitem =
-          '<div>'+
+          '<div class="list__item">'+
             '<article class="archive__item" itemscope itemtype="http://schema.org/CreativeWork">'+
-              '<div class="archive__item-teaser">'+
-                '<img src="'+store[ref].teaser+'" alt="">'+
-              '</div>'+
               '<h2 class="archive__item-title" itemprop="headline">'+
                 '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
               '</h2>'+
+              '<div class="archive__item-teaser">'+
+                '<img src="'+store[ref].teaser+'" alt="">'+
+              '</div>'+
               '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt+'</p>'+
             '</article>'+
           '</div>';
       }
       else{
     	  var searchitem =
-          '<div>'+
+          '<div class="list__item">'+
             '<article class="archive__item" itemscope itemtype="http://schema.org/CreativeWork">'+
               '<h2 class="archive__item-title" itemprop="headline">'+
                 '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
