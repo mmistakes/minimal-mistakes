a14a3f2227e75a3a97784342552fc5bc54f49011
nickgarlis <nickgarlis@gmail.com>
2017-11-27 21:46:00 +0200

    Add option to search in specific collections

--- assets/js/lunr-en.js
+++ assets/js/lunr-en.js
@@ -14,14 +14,16 @@ var idx = lunr(function () {
 {% for c in site.collections %}
   {% assign docs = c.docs %}
   {% for doc in docs %}
-    idx.add({
-      title: {{ doc.title | jsonify }},
-      excerpt: {{ doc.content | strip_html | truncatewords: 20 | jsonify }},
-      categories: {{ doc.categories | jsonify }},
-      tags: {{ doc.tags | jsonify }},
-      id: {{ count }}
-    });
-    {% assign count = count | plus: 1 %}
+    {% if doc.search == true %}
+      idx.add({
+        title: {{ doc.title | jsonify }},
+        excerpt: {{ doc.content | strip_html | truncatewords: 20 | jsonify }},
+        categories: {{ doc.categories | jsonify }},
+        tags: {{ doc.tags | jsonify }},
+        id: {{ count }}
+      });
+      {% assign count = count | plus: 1 %}
+    {% endif %}
   {% endfor %}
 {% endfor %}
 
@@ -34,22 +36,24 @@ var store = [
     {% endif %}
     {% assign docs = c.docs %}
     {% for doc in docs %}
-      {% if doc.header.teaser %}
-        {% capture teaser %}{{ doc.header.teaser }}{% endcapture %}
-      {% else %}
-        {% assign teaser = site.teaser %}
+      {% if doc.search == true %}
+        {% if doc.header.teaser %}
+          {% capture teaser %}{{ doc.header.teaser }}{% endcapture %}
+        {% else %}
+          {% assign teaser = site.teaser %}
+        {% endif %}
+        {
+          "title": {{ doc.title | jsonify }},
+          "url": {{ doc.url | absolute_url | jsonify }},
+          "excerpt": {{ doc.content | strip_html | truncatewords: 20 | jsonify }},
+          "teaser":
+            {% if teaser contains "://" %}
+              {{ teaser | jsonify }}
+            {% else %}
+              {{ teaser | absolute_url | jsonify }}
+            {% endif %}
+        }{% unless forloop.last and l %},{% endunless %}
       {% endif %}
-      {
-        "title": {{ doc.title | jsonify }},
-        "url": {{ doc.url | absolute_url | jsonify }},
-        "excerpt": {{ doc.content | strip_html | truncatewords: 20 | jsonify }},
-        "teaser":
-          {% if teaser contains "://" %}
-            {{ teaser | jsonify }}
-          {% else %}
-            {{ teaser | absolute_url | jsonify }}
-          {% endif %}
-      }{% unless forloop.last and l %},{% endunless %}
     {% endfor %}
   {% endfor %}]
 
