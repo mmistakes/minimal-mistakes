7e6a46c447aeda27784baafecf1ef0808c05c91c
Nick Garlis <nickgarlis@gmail.com>
2017-12-28 20:56:10 +0200

    Update Lunr (#1419)
    * Update Lunr to 2.1.5
    * Remove colons from query string
    
    Fixes #1419

--- assets/js/lunr-en.js
+++ assets/js/lunr-en.js
@@ -3,27 +3,27 @@ layout: null
 ---
 
 var idx = lunr(function () {
-  this.field('title', {boost: 10})
+  this.field('title')
   this.field('excerpt')
   this.field('categories')
   this.field('tags')
   this.ref('id')
-});
 
-{% assign count = 0 %}
-{% for c in site.collections %}
-  {% assign docs = c.docs | where_exp:'doc','doc.search != false' %}
-  {% for doc in docs %}
-    idx.add({
-      title: {{ doc.title | jsonify }},
-      excerpt: {{ doc.content | strip_html | truncatewords: 20 | jsonify }},
-      categories: {{ doc.categories | jsonify }},
-      tags: {{ doc.tags | jsonify }},
-      id: {{ count }}
-    });
-    {% assign count = count | plus: 1 %}
+  {% assign count = 0 %}
+  {% for c in site.collections %}
+    {% assign docs = c.docs | where_exp:'doc','doc.search != false' %}
+    {% for doc in docs %}
+      this.add({
+          title: {{ doc.title | jsonify }},
+          excerpt: {{ doc.content | strip_html | truncatewords: 20 | jsonify }},
+          categories: {{ doc.categories | jsonify }},
+          tags: {{ doc.tags | jsonify }},
+          id: {{ count }}
+      })
+      {% assign count = count | plus: 1 %}
+    {% endfor %}
   {% endfor %}
-{% endfor %}
+});
 
 console.log( jQuery.type(idx) );
 
@@ -56,8 +56,19 @@ var store = [
 $(document).ready(function() {
   $('input#search').on('keyup', function () {
     var resultdiv = $('#results');
-    var query = $(this).val();
-    var result = idx.search(query);
+    var query = $(this).val().toLowerCase().replace(":", "");
+    var result =
+      idx.query(function (q) {
+        query.split(lunr.tokenizer.separator).forEach(function (term) {
+          q.term(term, {  boost: 100 })
+          if(query.lastIndexOf(" ") != query.length-1){
+            q.term(term, {  usePipeline: false, wildcard: lunr.Query.wildcard.TRAILING, boost: 10 })
+          }
+          if (term != ""){
+            q.term(term, {  usePipeline: false, editDistance: 1, boost: 1 })
+          }
+        })
+      });
     resultdiv.empty();
     resultdiv.prepend('<p class="results__found">'+result.length+' {{ site.data.ui-text[site.locale].results_found | default: "Result(s) found" }}</p>');
     for (var item in result) {
