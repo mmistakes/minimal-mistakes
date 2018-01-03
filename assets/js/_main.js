e2b50a355fbf6bb88c6990f8353e5c30e8e21e54
Michael Rose <est.michael@gmail.com>
2018-01-03 14:20:41 -0500

    Fix stuck sidebar when resizing viewport
    Close #1447

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -24,6 +24,27 @@ $(document).ready(function() {
   // FitVids init
   $("#main").fitVids();
 
+  // Sticky sidebar
+  var stickySideBar = function() {
+    var show =
+      $(".author__urls-wrapper button").length === 0
+        ? $(window).width() > 1024 // width should match $large Sass variable
+        : !$(".author__urls-wrapper button").is(":visible");
+    if (show) {
+      // fix
+      $(".sidebar").addClass("sticky");
+    } else {
+      // unfix
+      $(".sidebar").removeClass("sticky");
+    }
+  };
+
+  stickySideBar();
+
+  $(window).resize(function() {
+    stickySideBar();
+  });
+
   // Follow menu drop down
   $(".author__urls-wrapper button").on("click", function() {
     $(".author__urls").toggleClass("is--visible");
