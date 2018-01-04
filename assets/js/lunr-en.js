6ee47103e4806cfa135ed7c4e014504c7f36e998
nickgarlis <nickgarlis@gmail.com>
2018-01-04 20:59:03 +0200

    Move store variable into a new file

--- assets/js/lunr-en.js
+++ assets/js/lunr-en.js
@@ -2,34 +2,6 @@
 layout: null
 ---
 
-var store = [
-  {% for c in site.collections %}
-    {% if forloop.last %}
-      {% assign l = true %}
-    {% endif %}
-    {% assign docs = c.docs | where_exp:'doc','doc.search != false' %}
-    {% for doc in docs %}
-      {% if doc.header.teaser %}
-        {% capture teaser %}{{ doc.header.teaser }}{% endcapture %}
-      {% else %}
-        {% assign teaser = site.teaser %}
-      {% endif %}
-      {
-        "title": {{ doc.title | jsonify }},
-        "excerpt": {{ doc.content | strip_html | strip_newlines | jsonify }},
-        "categories": {{ doc.categories | jsonify }},
-        "tags": {{ doc.tags | jsonify }},
-        "url": {{ doc.url | absolute_url | jsonify }},
-        "teaser":
-          {% if teaser contains "://" %}
-            {{ teaser | jsonify }}
-          {% else %}
-            {{ teaser | absolute_url | jsonify }}
-          {% endif %}
-      }{% unless forloop.last and l %},{% endunless %}
-    {% endfor %}
-  {% endfor %}]
-
 var idx = lunr(function () {
   this.field('title')
   this.field('excerpt')
