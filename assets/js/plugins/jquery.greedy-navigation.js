527d187e82f34e2d3192d25b15591f646a9bcda6
Michael Rose <est.michael@gmail.com>
2018-01-10 11:45:35 -0500

    Replace hardcoded `10` with actual button width

--- assets/js/plugins/jquery.greedy-navigation.js
+++ assets/js/plugins/jquery.greedy-navigation.js
@@ -25,7 +25,7 @@ $(document).ready(function() {
 
   function check() {
     // Get instant state
-    availableSpace = $vlinks.width() - 10;
+    availableSpace = $vlinks.width() - $btn.width();
     numOfVisibleItems = $vlinks.children().length;
     requiredSpace = breakWidths[numOfVisibleItems - 1];
 
