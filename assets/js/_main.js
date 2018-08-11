e51bae1f1ded986b97da1c4b24552f21fa3101d2
Daniel Julius Lasiman <dj.lasiman@gmail.com>
2018-08-11 19:56:47 +0700

    Fix smooth scroll breaking back button #1767

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -62,7 +62,28 @@ $(document).ready(function() {
   });
 
   // init smooth scroll
-  $("a").smoothScroll({ offset: -20 });
+
+  // Bind the hashchange event listener
+  $(window).bind("hashchange", function (event) {
+    $.smoothScroll({
+      // Replace '#/' with '#' to go to the correct target
+      scrollTarget: location.hash.replace(/^\#\/?/, "#"),
+      offset: -20
+    });
+  });
+  $('a[href*="#"]').bind("click", function (event) {
+    // Remove '#' from the hash.
+    var hash = this.hash.replace(/^#/, "");
+    if (this.pathname === location.pathname && hash) {
+      event.preventDefault();
+      // Change '#' (removed above) to '#/' so it doesn't jump without the smooth scrolling
+      location.hash = "#/" + hash;
+    }
+  });
+  // Trigger hashchange event on page load if there is a hash in the URL.
+  if (location.hash) {
+    $(window).trigger("hashchange");
+  }
 
   // add lightbox class to all image links
   $(
