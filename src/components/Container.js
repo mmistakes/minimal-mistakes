e442e28c7ee7f9b19aa6680271b3787707cfa1e4
nomangul <nomangul2001@gmail.com>
2019-04-25 02:53:43 +0500

    add UI for main container

---
+++ src/components/Container.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  margin: 7%;
+`;
