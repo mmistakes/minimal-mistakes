5c9beaecd0c8355bcf454f7733317b525f7c6093
nomangul <nomangul2001@gmail.com>
2019-04-25 02:53:57 +0500

    add UI for Heading

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Header = styled.h1`
+  font-size: calc(40px + 2vmin);
+  color: #000;
+  font-family: "Open Sans", sans-serif;
+  font-weight: 900;
+  display: inline-flex;
+  letter-spacing: -2px;
+`;
