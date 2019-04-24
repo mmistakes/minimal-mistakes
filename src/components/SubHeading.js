a50e58699deec40e651e9154354d203d3d990c4b
nomangul <nomangul2001@gmail.com>
2019-04-25 02:54:22 +0500

    add UI for SubHeading

---
+++ src/components/SubHeading.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const SubHeading = styled.div`
+  font-size: calc(15px + 2vmin);
+  color: #000;
+  font-family: "Open Sans", sans-serif;
+  font-weight: bold;
+  text-align: center;
+  margin: 0 10%;
+`;
