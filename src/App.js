e45b2699790a87da6649e8f0a1d7e8fc00737ed4
nomangul <nomangul2001@gmail.com>
2019-04-25 01:35:44 +0500

    remove unnecessary files and code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
