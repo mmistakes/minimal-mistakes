e944625768afffff8783a3f0e491059178ecaf17
nomangul <nomangul2001@gmail.com>
2019-04-25 02:52:41 +0500

    add UI for Heading, SubHeading, buttons and image

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from "react";
 import "./App.css";
+import { Header } from "./components/Header";
+import { Container } from "./components/Container";
+import { SubHeading } from "./components/SubHeading";
+import { Span } from "./components/Span";
+import { Button, ButtonContainer } from "./components/Button";
+import logo from "./assets/images/logo.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <img src={logo} alt="Logo" width={50} height={50} />
+      <Header>
+        <Span primary>Artificial</Span>
+        <Span>impact</Span>
+      </Header>
+      <SubHeading>
+        We are a not-for-profit initiative matching NGO's and international
+        organizations with A.I. developers with the goal of making the world a
+        better place.
+      </SubHeading>
+      <ButtonContainer>
+        <Button primary>I need help</Button>
+        <Button>I offer help</Button>
+      </ButtonContainer>
+    </Container>
   );
 }
 
