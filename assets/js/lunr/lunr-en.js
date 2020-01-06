03d3ac1e14437b1c85fa479440d9fa10bb63e595
MGidi <53842052+MGidi@users.noreply.github.com>
2020-01-06 14:12:14 +0000

    dir auto

--- assets/js/lunr/lunr-en.js
+++ assets/js/lunr/lunr-en.js
@@ -44,7 +44,7 @@ $(document).ready(function() {
       var ref = result[item].ref;
       if(store[ref].teaser){
         var searchitem =
-          '<div class="list__item">'+
+          '<div class="list__item"  dir="auto">'+
             '<article class="archive__item" itemscope itemtype="https://schema.org/CreativeWork">'+
               '<h2 class="archive__item-title" itemprop="headline">'+
                 '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
@@ -58,7 +58,7 @@ $(document).ready(function() {
       }
       else{
     	  var searchitem =
-          '<div class="list__item">'+
+          '<div class="list__item" dir="auto">'+
             '<article class="archive__item" itemscope itemtype="https://schema.org/CreativeWork">'+
               '<h2 class="archive__item-title" itemprop="headline">'+
                 '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
