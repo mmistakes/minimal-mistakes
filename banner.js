68b7e79c48c767002fb4ce189148c0942de36cb4
Jameson Buffmire <Jameson@Buffmire.com>
2020-07-21 01:25:51 -0700

    Update banner.js

--- banner.js
+++ banner.js
@@ -9,7 +9,7 @@ const date = `${dateObj.getFullYear()}-${padStart(
 )}-${padStart(dateObj.getDate())}`;
 const banner = `/*!
  * Minimal Mistakes Jekyll Theme ${pkg.version} by ${pkg.author}
- * Copyright 2013-${dateObj.getFullYear()} Michael Rose - mademistakes.com | @mmistakes
+ * Copyright 2013-${dateObj.getFullYear()} SignalSet - signalset.com | @signalsetslc
  * Licensed under ${pkg.license}
  */
 `;
