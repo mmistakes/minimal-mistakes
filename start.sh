87499515773470ac38634f4f1f6298035a1359e0
percyvega <percyvega@gmail.com>
2020-09-04 11:34:11 -0400

    Made page fonts smaller. Fixed bg title. Added space to description

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+# Note: for technical reasons, _config.yml is NOT reloaded automatically when used with jekyll serve.
+# If you make any changes to this file, please restart the server process for them to be applied.
+
 open -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome http://localhost:4000
 
 bundle exec jekyll serve --incremental
-
