ef67898aed60b0f807af0c597f6725e2ef19f42b
iBug <git@ibugone.com>
2021-02-09 22:19:18 +0800

    Add .no-copy for hiding the button, update docs

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -2,12 +2,12 @@
    jQuery plugin settings and other scripts
    ========================================================================== */
 
-$(document).ready(function() {
+$(document).ready(function () {
   // FitVids init
   $("#main").fitVids();
 
   // Sticky sidebar
-  var stickySideBar = function() {
+  var stickySideBar = function () {
     var show =
       $(".author__urls-wrapper button").length === 0
         ? $(window).width() > 1024 // width should match $large Sass variable
@@ -23,18 +23,18 @@ $(document).ready(function() {
 
   stickySideBar();
 
-  $(window).resize(function() {
+  $(window).resize(function () {
     stickySideBar();
   });
 
   // Follow menu drop down
-  $(".author__urls-wrapper button").on("click", function() {
+  $(".author__urls-wrapper button").on("click", function () {
     $(".author__urls").toggleClass("is--visible");
     $(".author__urls-wrapper button").toggleClass("open");
   });
 
   // Close search screen with Esc key
-  $(document).keyup(function(e) {
+  $(document).keyup(function (e) {
     if (e.keyCode === 27) {
       if ($(".initial-content").hasClass("is--hidden")) {
         $(".search-content").toggleClass("is--visible");
@@ -44,11 +44,11 @@ $(document).ready(function() {
   });
 
   // Search toggle
-  $(".search__toggle").on("click", function() {
+  $(".search__toggle").on("click", function () {
     $(".search-content").toggleClass("is--visible");
     $(".initial-content").toggleClass("is--hidden");
     // set focus on input
-    setTimeout(function() {
+    setTimeout(function () {
       $(".search-content input").focus();
     }, 400);
   });
@@ -58,7 +58,7 @@ $(document).ready(function() {
     offset: 20,
     speed: 400,
     speedAsDuration: true,
-    durationMax: 500
+    durationMax: 500,
   });
 
   // Gumshoe scroll spy init
@@ -77,7 +77,7 @@ $(document).ready(function() {
       reflow: true, // if true, listen for reflows
 
       // Event support
-      events: true // if true, emit custom events
+      events: true, // if true, emit custom events
     });
   }
 
@@ -99,73 +99,84 @@ $(document).ready(function() {
     gallery: {
       enabled: true,
       navigateByImgClick: true,
-      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+      preload: [0, 1], // Will preload 0 - before current, and 1 after the current image
     },
     image: {
-      tError: '<a href="%url%">Image #%curr%</a> could not be loaded.'
+      tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
     },
     removalDelay: 500, // Delay in milliseconds before popup is removed
     // Class that is added to body when popup is open.
     // make it unique to apply your CSS animations just to this exact popup
     mainClass: "mfp-zoom-in",
     callbacks: {
-      beforeOpen: function() {
+      beforeOpen: function () {
         // just a hack that adds mfp-anim class to markup
         this.st.image.markup = this.st.image.markup.replace(
           "mfp-figure",
           "mfp-figure mfp-with-anim"
         );
-      }
+      },
     },
     closeOnContentClick: true,
-    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+    midClick: true, // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
   });
 
   // Add anchors for headings
-  document.querySelector('.page__content').querySelectorAll('h1, h2, h3, h4, h5, h6').forEach(function(element) {
-    var id = element.getAttribute('id');
-    if (id) {
-      var anchor = document.createElement("a");
-      anchor.className = 'header-link';
-      anchor.href = '#' + id;
-      anchor.innerHTML = '<span class=\"sr-only\">Permalink</span><i class=\"fas fa-link\"></i>';
-      anchor.title = "Permalink";
-      element.appendChild(anchor);
-    }
-  });
+  document
+    .querySelector(".page__content")
+    .querySelectorAll("h1, h2, h3, h4, h5, h6")
+    .forEach(function (element) {
+      var id = element.getAttribute("id");
+      if (id) {
+        var anchor = document.createElement("a");
+        anchor.className = "header-link";
+        anchor.href = "#" + id;
+        anchor.innerHTML =
+          '<span class="sr-only">Permalink</span><i class="fas fa-link"></i>';
+        anchor.title = "Permalink";
+        element.appendChild(anchor);
+      }
+    });
 
   // Add copy button for <pre> blocks
-  var copyText = function(text) {
-    var isRTL = document.documentElement.getAttribute('dir') === 'rtl';
+  var copyText = function (text) {
+    if (document.queryCommandEnabled("copy") && navigator.clipboard) {
+      navigator.clipboard.writeText(textarea.value).then(
+        () => true, () => console.error("Failed to copy text to clipboard: " + text)
+      );
+      return true;
+    } else {
+      var isRTL = document.documentElement.getAttribute("dir") === "rtl";
 
-    var textarea = document.createElement('textarea');
-    textarea.className = "clipboard-helper";
-    textarea.style[isRTL ? 'right' : 'left'] = '-9999px';
-    // Move element to the same position vertically
-    var yPosition = window.pageYOffset || document.documentElement.scrollTop;
-    textarea.style.top = yPosition + "px";
+      var textarea = document.createElement("textarea");
+      textarea.className = "clipboard-helper";
+      textarea.style[isRTL ? "right" : "left"] = "-9999px";
+      // Move element to the same position vertically
+      var yPosition = window.pageYOffset || document.documentElement.scrollTop;
+      textarea.style.top = yPosition + "px";
 
-    textarea.setAttribute('readonly', '');
-    textarea.value = text;
-    document.body.appendChild(textarea);
+      textarea.setAttribute("readonly", "");
+      textarea.value = text;
+      document.body.appendChild(textarea);
 
-    var success = true;
-    try {
-      textarea.select();
-      success = document.execCommand("copy");
-    } catch (e) {
-      success = false;
+      var success = true;
+      try {
+        textarea.select();
+        success = document.execCommand("copy");
+      } catch (e) {
+        success = false;
+      }
+      textarea.parentNode.removeChild(textarea);
+      return success;
     }
-    textarea.parentNode.removeChild(textarea);
-    return success;
   };
 
-  var copyButtonEventListener = function(event) {
+  var copyButtonEventListener = function (event) {
     var thisButton = event.target;
 
     // Locate the <code> element
     var codeBlock = thisButton.nextElementSibling;
-    while (codeBlock && codeBlock.tagName.toLowerCase() !== 'code') {
+    while (codeBlock && codeBlock.tagName.toLowerCase() !== "code") {
       codeBlock = codeBlock.nextElementSibling;
     }
     if (!codeBlock) {
@@ -182,18 +193,20 @@ $(document).ready(function() {
     return copyText(codeBlock.innerText);
   };
 
-  document.querySelectorAll(".page__content pre > code").forEach(function(element, index, parentList) {
-    // Locate the <pre> element
-    var container = element.parentElement;
-    // Sanity check - don't add an extra button if there's already one
-    if (container.firstElementChild.tagName.toLowerCase() !== 'code') {
-      return;
-    }
-    var copyButton = document.createElement("button");
-    copyButton.title = "Copy to clipboard";
-    copyButton.className = "clipboard-copy-button";
-    copyButton.innerHTML = '<i class="far fa-copy"></i>';
-    copyButton.addEventListener("click", copyButtonEventListener);
-    container.prepend(copyButton);
-  });
+  document
+    .querySelectorAll(".page__content pre > code")
+    .forEach(function (element, index, parentList) {
+      // Locate the <pre> element
+      var container = element.parentElement;
+      // Sanity check - don't add an extra button if there's already one
+      if (container.firstElementChild.tagName.toLowerCase() !== "code") {
+        return;
+      }
+      var copyButton = document.createElement("button");
+      copyButton.title = "Copy to clipboard";
+      copyButton.className = "clipboard-copy-button";
+      copyButton.innerHTML = '<i class="far fa-copy"></i>';
+      copyButton.addEventListener("click", copyButtonEventListener);
+      container.prepend(copyButton);
+    });
 });
