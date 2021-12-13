6d812c8e80b0ee6da40d444254049a9d14ba3e33
CheolPark98 <96028198+CheolPark98@users.noreply.github.com>
2021-12-13 10:29:36 +0900

    Add files via upload

---
+++ _posts/2021-12-13-function_practice.md.py
@@ -0,0 +1,24 @@
+def open_account():
+    print("새로운 계좌가 개설되었습니다")
+
+def deposit(balance, money):
+    print("입금이 완료되었습니다. 잔액은 {} 원 입니다.".format(balance + money))
+    return balance + money
+
+def withdraw(balance, money):
+    if balance >= money:
+        print("출금이 완료되었습니다. 잔액은 {} 원 입니다".format(balance - money))
+    else:
+        print("출금이 완료되지 않았습니다. 잔액은 {} 원입니다.".format(balance))
+
+def withdraw_night(balance, money):
+    commission = 100
+    return commission, balance - money - commission
+    
+balance = 0    
+balance = deposit(balance, 1000)
+# balance = withdraw(balance, 2000)
+# balance = withdraw(balance, 500)
+# balance = withdraw_night(balance, 200)
+commission, balance = withdraw_night(balance, 500)
+print("수수료 {} 원이며, 잔액은 {} 원입니다".format(commission, balance))
