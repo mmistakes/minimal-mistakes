5a3bbd8384a0d4d802160e2f983536afeb10c80b
JMuff22 <jake.muff@mac.com>
2022-06-29 15:22:36 +0300

    Added additional splice content to display in search to stop overflow errors in html

--- assets/js/lunr/lunr-en.js
+++ assets/js/lunr/lunr-en.js
@@ -64,7 +64,7 @@ $(document).ready(function() {
               '<h2 class="archive__item-title" itemprop="headline">'+
                 '<a href="'+store[ref].url+'" rel="permalink">'+store[ref].title+'</a>'+
               '</h2>'+
-              '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt.split(" ").splice(0,20).join(" ")+'...</p>'+
+              '<p class="archive__item-excerpt" itemprop="description">'+store[ref].excerpt.split(" ").splice(0,100).join(" ")+'...</p>'+
             '</article>'+
           '</div>';
       }
