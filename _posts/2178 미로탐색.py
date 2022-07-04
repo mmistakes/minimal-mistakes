a0f1dca5808928569272dd7301cdca6a09b32523
donghyeok1 <07385@naver.com>
2022-07-04 13:35:17 +0900

    update_2178

--- _posts/2178 미로탐색.py
+++ _posts/2178 미로탐색.py
@@ -1,10 +1,34 @@
 import sys
+from collections import deque
 
 N, M = map(int, sys.stdin.readline().split())
 
-maze = []
+graph = []
+queue = deque()
+
 
 for i in range(N):
-    maze.append(list(sys.stdin.readline().rstrip()))
+    graph.append(list(map(int, sys.stdin.readline().rstrip())))
     
-print(maze)
+# for i in range(N):
+#     for j in range(M):
+#         if graph[i][j] == 1:
+#             queue.append([i, j])
+            
+dx = [-1, 0, 0, 1]
+dy = [0, -1, 1, 0]
+
+queue.append([0, 0])
+
+while queue:
+    x, y = queue.popleft()
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx >= 0 and ny >= 0 and nx <= N - 1 and ny <= M - 1:
+            if graph[nx][ny] != 0 and graph[nx][ny] == 1:
+                graph[nx][ny] += graph[x][y]
+                queue.append([nx, ny])
+                
+            
+print(graph[N - 1][M - 1])
