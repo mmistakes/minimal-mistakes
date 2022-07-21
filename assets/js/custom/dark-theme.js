f6ba88f96baf4fd023eb01e05904177f24966065
김찬영 <mz_chansm@woowafriends.com>
2022-07-21 10:50:13 +0900

    darkmode 수정

--- assets/js/custom/dark-theme.js
+++ assets/js/custom/dark-theme.js
@@ -16,14 +16,6 @@ const dateValidDarkMode = () => {
   return 6 < hours < 22;
 };
 
-const disqusColorReload = () => {
-  var d = document,
-    s = d.createElement("script");
-  s.src = "https://SITE_URL.disqus.com/embed.js"; // Replace SITE_URL with your site's URL
-  s.setAttribute("data-timestamp", +new Date());
-  (d.head || d.body).appendChild(s);
-};
-
 const localStorageSetting = (key, removeType, setType) => {
   localStorage.removeItem(key, removeType);
   localStorage.setItem(key, setType);
@@ -32,23 +24,20 @@ const switchTheme = (e) => {
   if (e.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
     localStorageSetting("theme", "light", "dark");
-    const link = document.location.href;
-    console.log(link);
-
-    disqusColorReload();
   } else {
     document.documentElement.setAttribute("data-theme", "light");
     localStorageSetting("theme", "dark", "light");
-    disqusColorReload();
   }
 };
 
 toggleSwitch.addEventListener("change", switchTheme, false);
 
-window.addEventListener("load", function () {
-  const setMode = dateValidDarkMode() ? "light" : "dark";
-  const removeMode = setMode === "light" ? "dark" : "light";
-  localStorageSetting("theme", removeMode, setMode);
-  document.documentElement.setAttribute("data-theme", setMode);
-  console.log(50, localStorage.getItem("theme"), removeMode, setMode);
+window.addEventListener("DOMContentLoaded", function () {
+  const mode = localStorage.getItem("theme");
+  if (mode) {
+    toggleSwitch.checked = mode === "dark";
+  } else {
+    toggleSwitch.checked = true;
+    document.documentElement.setAttribute("data-theme", "dark");
+  }
 });
