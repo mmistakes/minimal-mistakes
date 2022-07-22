8950e91434b46d00521c05af61489a2d09653911
donghyeok1 <95459089+donghyeok1@users.noreply.github.com>
2022-07-23 01:12:54 +0900

    update_1967

--- _posts/1967 트리의 지름.py
+++ _posts/1967 트리의 지름.py
@@ -1,44 +1,44 @@
 ##########################################################
 # 플로이드 워셜 풀이
 
-import sys
+# import sys
 
-N = int(sys.stdin.readline())
+# N = int(sys.stdin.readline())
 
-INF = 10 ** 9
+# INF = 10 ** 9
 
-graph = [[INF] * (N + 1) for _ in range(N + 1)]
+# graph = [[INF] * (N + 1) for _ in range(N + 1)]
 
 
-for i in range(1, N + 1):
-    for j in range(1, N + 1):
-        if i == j:
-            graph[i][j] = 0
+# for i in range(1, N + 1):
+#     for j in range(1, N + 1):
+#         if i == j:
+#             graph[i][j] = 0
 
-for i in range(N - 1):
-    a, b, c = map(int, sys.stdin.readline().split())
-    graph[a][b] = c
-    graph[b][a] = c
+# for i in range(N - 1):
+#     a, b, c = map(int, sys.stdin.readline().split())
+#     graph[a][b] = c
+#     graph[b][a] = c
 
-for k in range(1, N + 1):
-    for i in range(1, N + 1):
-        for j in range(1, N + 1):
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
-print(graph)
-max_list = []
-max_val = 0
+# for k in range(1, N + 1):
+#     for i in range(1, N + 1):
+#         for j in range(1, N + 1):
+#             graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+# print(graph)
+# max_list = []
+# max_val = 0
 
-for i in range(1, N + 1):
-    for j in graph[i]:
-        if j == INF:
-            continue
-        else:
-            if max_val < j:
-                max_val = j
-    max_list.append(max_val)
-    max_val = 0
+# for i in range(1, N + 1):
+#     for j in graph[i]:
+#         if j == INF:
+#             continue
+#         else:
+#             if max_val < j:
+#                 max_val = j
+#     max_list.append(max_val)
+#     max_val = 0
 
-print(max(max_list))
+# print(max(max_list))
 
 ##########################################################
 # DFS 풀이
