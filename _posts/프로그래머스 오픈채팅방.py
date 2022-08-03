f597995a4a2affcdd745ea13d0e2137b793e30ca
donghyeok1 <95459089+donghyeok1@users.noreply.github.com>
2022-08-03 23:45:20 +0900

    update_test

--- _posts/프로그래머스 오픈채팅방.py
+++ _posts/프로그래머스 오픈채팅방.py
@@ -24,10 +24,4 @@ def solution(record):
         elif flag == "L":
             a += name_list[id] + "님이 나갔습니다."
             answer.append(a)
-
-    
     return answer
-
-record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
-
-print(solution(record))
