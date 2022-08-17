79cae7874b10aa72c2302b44f69e7075101acf0f
donghyeok1 <95459089+donghyeok1@users.noreply.github.com>
2022-08-17 16:31:24 +0900

    update_1022,2251,django_start

--- _posts/2251 물통.py
+++ _posts/2251 물통.py
@@ -93,63 +93,25 @@ def bfs():
 bfs()
         
                 
-# import sys
-# from collections import deque
-
-# # x, y의 경우의 수 저장
-# def pour(x, y):
-#     if not visited[x][y]:
-#         visited[x][y] = True
-#         q.append((x, y))
-
-# def bfs():
-
-#     while q:
-#         # x : a물통의 물의 양, y : b물통의 물의 양, z : c물통의 물의 양
-#         x, y = q.popleft()
-#         z = c - x - y
-
-#         # a 물통이 비어있는 경우 c 물통에 남아있는 양 저장
-#         if x == 0:
-#             answer.append(z)
-
-#         # x -> y
-#         water = min(x, b-y)
-#         pour(x - water, y + water)
-#         # x -> z
-#         water = min(x, c-z)
-#         pour(x - water, y)
-#         # y -> x
-#         water = min(y, a-x)
-#         pour(x + water, y - water)
-#         # y -> z
-#         water = min(y, c-z)
-#         pour(x, y - water)
-#         # z -> x
-#         water = min(z, a-x)
-#         pour(x + water, y)
-#         # z -> y
-#         water = min(z, b-y)
-#         pour(x, y + water)
-
-
-# # 입력(리터 범위)
-# a, b, c = map(int, sys.stdin.readline().split())
-
-# # 경우의 수를 담을 큐
-# q = deque()
-# q.append((0, 0))
-
-# # 방문 여부(visited[x][y])
-# visited = [[False] * (b+1) for _ in range(a+1)]
-# visited[0][0] = True
-
-# # 답을 저장할 배열
-# answer = []
-
-# bfs()
-
-# # 출력
-# answer.sort()
-# for i in answer:
-#     print(i, end=" ")
+# c가 0이 아닐때
+# 	c->a
+# 		c + a 가 A를 초과할 경우
+# 		c + a 가 A를 초과하지 않을 경우
+# 	c->b
+# 		c + b 가 B를 초과할 경우
+# 		c + b 가 B를 초과하지 않을 경우
+# b가 0이 아닐때
+# 	b->a
+# 		b + a 가 A를 초과할 경우
+# 		b + a 가 A를 초과하지 않을 경우
+# 	b->c
+# 		b + c 가 C를 초과할 경우
+# 		b + c 가 C를 초과하지 않을 경우
+# a가 0이 아닐때
+# 	a->c
+# 		a + c 가 C를 초과할 경우
+# 		a + c 가 C를 초과하지 않을 경우
+# 	a->b
+# 		a + b 가 B를 초과할 경우
+# 		a + b 가 B를 초과하지 않을 경우
+	
