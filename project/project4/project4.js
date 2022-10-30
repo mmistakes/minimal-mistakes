dbbff31d3342ce25fe1c2e58a44952ccdd7dcc95
catriever <ekzm849@naver.com>
2022-10-30 22:02:14 +0900

    a

--- project/project4/project4.js
+++ project/project4/project4.js
@@ -1,7 +1,27 @@
-const title = document.querySelector("div.hello h1");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-function handleTitleClick() {
-  console.log("title was clicked!");
+function onLoginSubmit(event) {
+  event.preventDefault();
+  localStorage.setItem(USERNAME_KEY, loginInput.value);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  paintGreetings();
+}
+function paintGreetings() {
+  const username = localStorage.getItem(USERNAME_KEY);
+  greeting.innerText = `Hello! ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-title.addEventListener("click", handleTitleClick);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  const username = loginInput.value;
+  paintGreetings();
+}
