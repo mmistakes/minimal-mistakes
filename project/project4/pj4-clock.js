2a0914d5ad251fb7554f0aa76ef68ee263c389a4
catriever <ekzm849@naver.com>
2022-10-31 22:17:02 +0900

    a

---
+++ project/project4/pj4-clock.js
@@ -0,0 +1,12 @@
+const clock = document.querySelector("#clock");
+
+function getClock() {
+  const date = new Date();
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
