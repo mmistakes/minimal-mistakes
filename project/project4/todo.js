b6722ef4154044a401b2b0df4a26f493f99c52bb
catriever <ekzm849@naver.com>
2022-11-04 18:55:31 +0900

    d

--- project/project4/todo.js
+++ project/project4/todo.js
@@ -1,2 +1,20 @@
-ss;
-ss;
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  li.appendChild(span);
+  span.innerText = newTodo;
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  paintToDo(newTodo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
