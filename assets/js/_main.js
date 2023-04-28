edbfefc35512c01d6dd677c06c1c560752e1f03c
Bhanu Thakur <bhanu.th@outlook.com>
2023-04-28 14:10:23 +0530

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -134,3 +134,17 @@ $(function() {
     }
   });
 });
+
+
+
+function detectmob() {
+  if(window.innerWidth <= 800 || window.innerHeight <= 600) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+if (detectmob()){
+top.location.href="portfolio";
+}
