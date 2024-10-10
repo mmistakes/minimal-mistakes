5693b03bc6476a14ce2c82c5d1022fbb0ccfea9f
duhokim0901 <kimvj200@gmail.com>
2024-10-11 02:20:08 +0900

    업데이트

--- assets/js/magnific-popup-setting.js
+++ assets/js/magnific-popup-setting.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     // 2.1. id가 magnific인 경우에만 magnific-popup 적용.
     //$('.page__content img[id="magnific"]').wrap( function(){
-    $('.page__content img').wrap( function(){    
+    $('.page__content img').wrap( function(){
 		
         // 2.2. magnific-popup 옵션 설정.
         $(this).magnificPopup({
@@ -19,6 +19,5 @@ $(document).ready(function() {
 				
         // 2.4. 이미지를 감쌀 태그 설정.
         return '<a href="' + $(this).attr('src') + '" style="width:' + $(this).attr('width') +'px; float: left;"><figure> </figure>' + '<figcaption style="text-align: center;" class="caption">' + $(this).attr('alt') + '</figcaption>' + '</a>';
-        //return '<a href="' + $(this).attr('src')+ '></a>';
     });
 });
