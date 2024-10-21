dd9f10f051fc708d6595816a4dcde4fad5a0a081
tomkdefra <tom.kirkpatrick@defra.gov.uk>
2024-10-21 12:07:24 +0100

    clean out deploy branch ready for static content

--- _sass/minimal-mistakes/_copyright.scss
+++
@@ -1,6 +0,0 @@
-/*!
- * Minimal Mistakes Jekyll Theme 4.26.2 by Michael Rose
- * Copyright 2013-2024 Michael Rose - mademistakes.com | @mmistakes
- * Free for personal and commercial use under the MIT license
- * https://github.com/mmistakes/minimal-mistakes/blob/master/LICENSE
- */
